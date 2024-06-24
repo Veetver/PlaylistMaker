9c1bdc303d937d1ca3007205847b257983cb7a53
Eduard Khabiev <khabiev2012@yandex.ru>
2024-06-24 20:38:16 +0300

    edited styles and themes; added searchEditText;

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,10 +1,17 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,25 @@ class SearchActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_search)
+        val clearTextBtn = findViewById<ImageView>(R.id.clear_text_btn)
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+
+        toolbar.setNavigationOnClickListener {
+            this.finish()
+        }
+
+        clearTextBtn.setOnClickListener {
+            searchEditText.setText(getString(R.string.empty))
+
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        }
+
+        searchEditText.doAfterTextChanged {
+            clearTextBtn.isVisible = !searchEditText.text.isNullOrEmpty()
         }
     }
 }
