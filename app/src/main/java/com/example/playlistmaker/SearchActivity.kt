0a9a55fac47537c0f227bf705d5fc9a9a3140d4a
Eduard Khabiev <khabiev2012@yandex.ru>
2024-07-01 02:09:02 +0300

    removed multiple themes; removed empty string resource.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,7 +2,6 @@ package com.example.playlistmaker
 
 import android.content.Context
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
@@ -39,7 +38,7 @@ class SearchActivity : AppCompatActivity() {
         }
 
         clearTextBtn.setOnClickListener {
-            searchEditText.setText(getString(R.string.empty))
+            searchEditText.text = null
 
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
